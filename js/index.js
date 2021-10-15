03cd3fe56e655ee6021e50733345edc1b5df4f95
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-10-15 14:38:15 +0200

    Add first javascript

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const buttonAnswer = document.querySelector('.js-card__button')
+const cardAnswer = document.querySelector('.js-card__answer')
+
+buttonAnswer.addEventListener('click', () => {
+    cardAnswer.classList.toggle('hidden')
+})
