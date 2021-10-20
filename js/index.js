4ddfca03dce0193fd3d53a96706733c8d4ad62bf
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-10-20 11:35:29 +0200

    Add tooling

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ const buttonAnswer = document.querySelector('.js-card__button')
 const cardAnswer = document.querySelector('.js-card__answer')
 
 buttonAnswer.addEventListener('click', () => {
-    cardAnswer.classList.toggle('hidden')
+  cardAnswer.classList.toggle('hidden')
 })
